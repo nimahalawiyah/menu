2817a16fac64f8bd9811ea78600010d239bdb70d
na99 <iamnimahalawiyah@gmail.com>
2018-05-31 14:51:22 +0700

    Delete loginservice.js

--- loginservice.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-myapp.factory('loginservice',function($http,$location){
-	return{
-		islogged:function(){
-			var $checksession=$http.post('data/check.php');
-			return $checksession;
-		}
-	}
-});
